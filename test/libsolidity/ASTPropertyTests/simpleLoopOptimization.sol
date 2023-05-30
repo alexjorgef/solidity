dfedba9445aebbe682ee874c75bf4b2da6dbb41a
Matheus Aguiar <matheus.pit@gmail.com>
2023-05-30 12:07:10 -0300

    fixup: cleanup test

--- test/libsolidity/ASTPropertyTests/simpleLoopOptimization.sol
+++ test/libsolidity/ASTPropertyTests/simpleLoopOptimization.sol
@@ -1,5 +1,4 @@
 contract C {
-    /// TestCaseFunctionSelector : functionSelector
   function f() public pure {
      /// TestCase1: isSimpleCounterLoop
      for(uint i = 0; i < 42; ++i) {
@@ -8,23 +7,7 @@ contract C {
      for(uint i = 1; i < 42; i = i * 2) {
      }
   }
-  function g() public pure  {
-      /// TestAssignment: initialValue.leftExpression.typeDescriptions.typeString
-      uint x = 1 + 1;
-      /// TestCase4: condition.operator
-      if (x < 0)
-        revert("Error!");
-  }
-}
-
-contract D {
-    /// TestCase3: kind
-    function f() public pure {}
 }
 // ----
-// TestAssignment: int_const 1
 // TestCase1: true
 // TestCase2: false
-// TestCase3: function
-// TestCaseFunctionSelector: 26121ff0
-// TestCase4: <
